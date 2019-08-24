212ca4bb8f443dd07683e0d94031aef4d096ead3
Picho <ucardesarollo@gmail.com>
2019-08-24 18:01:24 -0500

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,12 +12,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
         //$this->call(RoleTableSeeder::class);
-       // $this->call(tallerservicios::class);
+        //$this->call(tallerservicios::class);
         //$this->call(sucursal::class);
-       // $this->call(vehiculosucursal::class);
         //$this->call(Vehiculo::class);
+        //$this->call(vehiculosucursal::class);
         //$this->call(servicios_extra::class);
     }
 }
