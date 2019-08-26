1c94925ccf12016bede8a05fa9753de7ee0b5cd1
Picho <ucardesarollo@gmail.com>
2019-08-26 03:44:45 -0500

    clientes frecuentes

--- resources/views/gerente/clientes/ver_clientes.blade.php
+++ resources/views/gerente/clientes/ver_clientes.blade.php
@@ -28,77 +28,165 @@
 
     <!-- Main content -->
     <section class="content">
-        @if (count($clientes)==0)
-        <div style="display: none;">
-          <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-warning" id="<?php 
-          echo "boton";
-        ?>">
-              Cancelar
-            </button>
-          </div>
-          @endif 
-          
-              <!-- /.box-header -->
-              <div class="box-body">
-                  <table id="example" class="display nowrap " style="width:100%">
-                      <thead>
-                          <tr>
-                              <th style="text-align: center">Credencial</th>
-                              <th style="text-align: center">Pasaporte</th>
-                              <th style="text-align: center">Nombre</th>
-                              <th style="text-align: center">Apellido Paterno</th>                              
-                              <th style="text-align: center">Apellido Materno</th>   
-                              <th style="text-align: center">Fecha Nacimiento</th>
-                              <th style="text-align: center">Nacionalidad</th>
-                              <th style="text-align: center">Correo</th>
-                              <th style="text-align: center">Teléfono</th>
-                              <th style="text-align: center">País</th>
-                              <th style="text-align: center">Estado</th>
-                              <th style="text-align: center">Ciudad</th>
-                              <th style="text-align: center">Colonia</th>
-                              <th style="text-align: center">Calle</th>
-                              <th style="text-align: center">Fecha Alta</th>
-                              
-                          </tr>
-                      </thead>
-                      <tbody>
-                           @foreach ($clientes as $cliente)                      
-                      <tr>
-                            @if ($cliente->credencial==null)
-                            <td style="text-align: center">---------</td>
-                            @else
-                            <td style="text-align: center">{{$cliente->credencial}}</td>
-                            @endif 
-                            @if ($cliente->pasaporte==null)
-                            <td style="text-align: center">---------</td>
-                            @else
-                            <td style="text-align: center">{{$cliente->pasaporte}}</td>
-                            @endif 
-                        <td style="text-align: center">{{$cliente->nombre}}</td>
-                        <td style="text-align: center">{{$cliente->primer_apellido}}</td>
-                        @if ($cliente->segundo_apellido==null)
-                        <td style="text-align: center">---------</td>
-                        @else
-                        <td style="text-align: center">{{$cliente->segundo_apellido}}</td>
+
+        <div class="row">
+            <!-- Left col -->
+            <section class="col-lg-12 connectedSortable">
+              <!-- Custom tabs (Charts with tabs)-->
+              <div class="nav-tabs-custom">
+                <!-- Tabs within a box -->
+                <ul class="nav nav-tabs pull-right">
+                 
+                  <li><a href="#sales-chart" data-toggle="tab">Frecuentes</a></li>
+                  <li class="active"><a href="#revenue-chart" data-toggle="tab">Todos</a></li>
+                  <li class="pull-left header"><i class="fa fa-inbox"></i>Clientes </li>
+                </ul>
+                <div class="tab-content no-padding">
+                  <!-- Morris chart - Sales -->
+                  <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height:auto;   ">
+                      @if (count($clientes)==0)
+                      <div style="display: none;">
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modal-warning" id="<?php 
+                        echo "boton";
+                      ?>">
+                          </button>
+                        </div>
                         @endif 
-                        <td style="text-align: center">{{$cliente->fecha_nacimiento}}</td>
-                        <td style="text-align: center">{{$cliente->nacionalidad}}</td>
-                        <td style="text-align: center">{{$cliente->correo}}</td>
-                        <td style="text-align: center">{{$cliente->telefono}}</td>
-                        <td style="text-align: center">{{$cliente->pais}}</td>
-                        <td style="text-align: center">{{$cliente->estado}}</td>
-                        <td style="text-align: center">{{$cliente->ciudad}}</td>
-                        <td style="text-align: center">{{$cliente->colonia}}</td>
-                        <td style="text-align: center">{{$cliente->calle}}</td>
-                        <td style="text-align: center">{{$cliente->created_at}}</td>
-                      </tr> 
-                @endforeach
-                      
-                      </tbody>
-                  </table>
+
+                        <table id="example" class="display nowrap " style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th style="text-align: center;background: lightblue">Credencial</th>
+                                    <th style="text-align: center;background: lightblue">Pasaporte</th>
+                                    <th style="text-align: center;background: lightblue">Nombre</th>
+                                    <th style="text-align: center;background: lightblue">Apellido Paterno</th>                              
+                                    <th style="text-align: center;background: lightblue">Apellido Materno</th>   
+                                    <th style="text-align: center;background: lightblue">Fecha Nacimiento</th>
+                                    <th style="text-align: center;background: lightblue">Nacionalidad</th>
+                                    <th style="text-align: center;background: lightblue">Correo</th>
+                                    <th style="text-align: center;background: lightblue">Teléfono</th>
+                                    <th style="text-align: center;background: lightblue">País</th>
+                                    <th style="text-align: center;background: lightblue">Estado</th>
+                                    <th style="text-align: center;background: lightblue">Ciudad</th>
+                                    <th style="text-align: center;background: lightblue">Colonia</th>
+                                    <th style="text-align: center;background: lightblue">Calle</th>
+                                    <th style="text-align: center;background: lightblue">Fecha Alta</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                 @foreach ($clientes as $cliente)                      
+                            <tr>
+                                  @if ($cliente->credencial==null)
+                                  <td style="text-align: center">---------</td>
+                                  @else
+                                  <td style="text-align: center">{{$cliente->credencial}}</td>
+                                  @endif 
+                                  @if ($cliente->pasaporte==null)
+                                  <td style="text-align: center">---------</td>
+                                  @else
+                                  <td style="text-align: center">{{$cliente->pasaporte}}</td>
+                                  @endif 
+                              <td style="text-align: center">{{$cliente->nombre}}</td>
+                              <td style="text-align: center">{{$cliente->primer_apellido}}</td>
+                              @if ($cliente->segundo_apellido==null)
+                              <td style="text-align: center">---------</td>
+                              @else
+                              <td style="text-align: center">{{$cliente->segundo_apellido}}</td>
+                              @endif 
+                              <td style="text-align: center">{{$cliente->fecha_nacimiento}}</td>
+                              <td style="text-align: center">{{$cliente->nacionalidad}}</td>
+                              <td style="text-align: center">{{$cliente->correo}}</td>
+                              <td style="text-align: center">{{$cliente->telefono}}</td>
+                              <td style="text-align: center">{{$cliente->pais}}</td>
+                              <td style="text-align: center">{{$cliente->estado}}</td>
+                              <td style="text-align: center">{{$cliente->ciudad}}</td>
+                              <td style="text-align: center">{{$cliente->colonia}}</td>
+                              <td style="text-align: center">{{$cliente->calle}}</td>
+                              <td style="text-align: center">{{$cliente->created_at}}</td>
+                            </tr> 
+                      @endforeach
+                            
+                            </tbody>
+                        </table>
+                  </div>
+
+
+                  {{-- SECCION DE CLIENTES FRECUENTES--}}
+                  <div class="chart tab-pane" id="sales-chart" style="position: relative; height:auto;">
+                      CLIENTES FRECUENTES
+                      <?php $frecuentes = DB::table('reservacions')                                    
+                                    ->join('clientes','idCliente','=','reservacions.id_cliente')
+                                    ->select(DB::raw('count(*) as cantidad, reservacions.id_cliente,clientes.*'))
+                                      ->groupBy('reservacions.id_cliente')
+                                      ->orderBy('cantidad','desc')
+                                      //->limit(10)
+                                      ->get()
+                                      ; 
+                                    //  $frecuentes=array_reverse($frecuentes,true)
+                      ?>
+                        <table id="example2" class="display nowrap " style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th style="text-align: center;background: lightblue">Frecuencia</th>
+                                    <th style="text-align: center;background: lightblue">Credencial</th>
+                                    <th style="text-align: center;background: lightblue">Pasaporte</th>
+                                    <th style="text-align: center;background: lightblue">Nombre</th>
+                                    <th style="text-align: center;background: lightblue">Apellido Paterno</th>                              
+                                    <th style="text-align: center;background: lightblue">Apellido Materno</th>   
+                                    <th style="text-align: center;background: lightblue">Fecha Nacimiento</th>
+                                    <th style="text-align: center;background: lightblue">Nacionalidad</th>
+                                    <th style="text-align: center;background: lightblue">Correo</th>
+                                    <th style="text-align: center;background: lightblue">Teléfono</th>
+                                    <th style="text-align: center;background: lightblue">País</th>
+                                    <th style="text-align: center;background: lightblue">Estado</th>
+                                    <th style="text-align: center;background: lightblue">Ciudad</th>
+                                    <th style="text-align: center;background: lightblue">Colonia</th>
+                                    <th style="text-align: center;background: lightblue">Calle</th>
+                                    <th style="text-align: center;background: lightblue">Fecha Alta</th>
+                                    
+                                </tr>
+                            </thead>
+                            <tbody>
+                                 @foreach ($frecuentes as $fre)                      
+                            <tr>
+                                <td style="text-align: center">{{$fre->cantidad}}</td>
+                                  @if ($fre->credencial==null)
+                                  <td style="text-align: center">---------</td>
+                                  @else
+                                  <td style="text-align: center">{{$fre->credencial}}</td>
+                                  @endif 
+                                  @if ($fre->pasaporte==null)
+                                  <td style="text-align: center">---------</td>
+                                  @else
+                                  <td style="text-align: center">{{$fre->pasaporte}}</td>
+                                  @endif 
+                              <td style="text-align: center">{{$fre->nombre}}</td>
+                              <td style="text-align: center">{{$fre->primer_apellido}}</td>
+                              @if ($fre->segundo_apellido==null)
+                              <td style="text-align: center">---------</td>
+                              @else
+                              <td style="text-align: center">{{$fre->segundo_apellido}}</td>
+                              @endif 
+                              <td style="text-align: center">{{$fre->fecha_nacimiento}}</td>
+                              <td style="text-align: center">{{$fre->nacionalidad}}</td>
+                              <td style="text-align: center">{{$fre->correo}}</td>
+                              <td style="text-align: center">{{$fre->telefono}}</td>
+                              <td style="text-align: center">{{$fre->pais}}</td>
+                              <td style="text-align: center">{{$fre->estado}}</td>
+                              <td style="text-align: center">{{$fre->ciudad}}</td>
+                              <td style="text-align: center">{{$fre->colonia}}</td>
+                              <td style="text-align: center">{{$fre->calle}}</td>
+                              <td style="text-align: center">{{$fre->created_at}}</td>
+                            </tr> 
+                      @endforeach
+                            
+                            </tbody>
+                        </table>
+                  </div>
+                </div>
               </div>
-             
-     
+            </div>          
       </section>
    
       <div class="modal modal-warning fade" id="modal-warning">
@@ -145,6 +233,17 @@
               } );
           } );
          </script>
+         <script>
+            $(document).ready(function() {
+                 $('#example2').DataTable( {
+                   "scrollX": true,
+                   "order": [[ 3, "desc" ]],
+                   "language": {
+                     "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json"
+                   },                   
+                 } );
+             } );
+            </script>
          <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
          <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
      @endsection
