2eab540a68f9dfe35cd841764a717e4ecad61a7e
Picho <ucardesarollo@gmail.com>
2019-08-26 23:49:52 -0500

    Error admin route corregido

--- resources/views/gerente/inicio.blade.php
+++ resources/views/gerente/inicio.blade.php
@@ -58,7 +58,7 @@
             <div class="icon">
               <i class="ion ion-person-add"></i>
             </div>
-            <a href="{{ route('cliente')}}" class="small-box-footer">Ver mas<i class="fa fa-arrow-circle-right"></i></a>
+            <a href="{{ route('cliente.index')}}" class="small-box-footer">Ver mas<i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
         <!-- ./col -->
