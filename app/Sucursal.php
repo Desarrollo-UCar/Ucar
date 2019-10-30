a88c1aa7683dbeefb0056bc4ffc44811a925022f
Picho <ucardesarollo@gmail.com>
2019-10-30 09:18:46 -0600

    link susucursal

--- app/Sucursal.php
+++ app/Sucursal.php
@@ -8,6 +8,6 @@ class Sucursal extends Model
 {
     public $primaryKey ='idsucursal';
     protected $fillable = [
-        'foto','foto1','foto2','nombre','codigopostal','estado','municipio','colonia','calle','numero','telefono','status'
+        'foto','foto1','foto2','nombre','link','codigopostal','estado','municipio','colonia','calle','numero','telefono','status'
     ];
 }
